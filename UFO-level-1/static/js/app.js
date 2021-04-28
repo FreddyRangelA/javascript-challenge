513e4e8493ad376dccbd26e8a9831f63a4201bd8
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-04-28 12:22:19 -0500

    table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
+tableData.forEach(function(ufoReport) {
+      console.log(ufoReport);
+      var row = tbody.append("tr");
+      Object.entries(ufoReport).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+          });
+        
+    });
+    
 // YOUR CODE HERE!
